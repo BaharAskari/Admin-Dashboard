0a9444f33a0810b3e287e2153bdd2556aa7ede51
BaharAskari <bahar.askari97@gmail.com>
2022-07-21 18:38:33 +0430

    Admin

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+const menuBtn = document.getElementById('menu-btn')
+const sideMenu = document.querySelector('aside')
+const closeBtn = document.getElementById('close-btn')
+const themeToggler = document.querySelector('.theme-toggler')
+
+menuBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'block'
+})
+
+closeBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'none'
+})
+
+//change theme
+themeToggler.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme-variables')
+
+  themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+  themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+})
+
+
+//fill table with some orders
